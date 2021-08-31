36d9dbcf64383a5a7c9bc9b73d608b775639942d
Saumya Lahera <saumyalahera@gmail.com>
2021-08-31 02:01:12 -0500

    Added the project

---
+++ YinzCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YinzCam
+//
+//  Created by Saumya Lahera on 8/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
